aefe8af9e19f0816522d72ae8943dbdbe905c505
Andrei Alekhin <andriyshkoy@yandex.ru>
2025-06-25 02:12:26 +0700

    codestyle fix

--- billing_daemon/main.py
+++ billing_daemon/main.py
@@ -1,13 +1,7 @@
 from multiprocessing import Process
 
-
-from core.config import settings
-
 from .rq_worker import run_worker
 from .scheduler import bootstrap_schedule
-
-
-
 
 if __name__ == "__main__":
     worker = Process(target=run_worker, daemon=True)
